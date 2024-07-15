3d3de8552ab535541be23ff22cd200109f315d77
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-15 10:15:23 -0400

    Update esp_adf_button.h

--- components/esp_adf/button/esp_adf_button.h
+++ components/esp_adf/button/esp_adf_button.h
@@ -23,12 +23,12 @@ class ButtonHandler {
   static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
   static void button_event_handler(void *handler_args, esp_event_base_t base, int32_t id, void *event_data);
   static void handle_button_event(int32_t id, int32_t event_type);  // Add this declaration
-  static void volume_up();
-  static void volume_down();
-  static void handle_mode_button();
-  static void handle_play_button();
-  static void handle_set_button();
-  static void handle_rec_button();
+  void volume_up();
+  void volume_down();
+  void handle_mode_button();
+  void handle_play_button();
+  void handle_set_button();
+  void handle_rec_button();
 
   void set_volume(int volume);
   int get_current_volume();
