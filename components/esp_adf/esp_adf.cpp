ff1dafec655bf3817976e2daa37ff55846d78ec8
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-15 19:56:41 -0400

    Update esp_adf.cpp

--- components/esp_adf/esp_adf.cpp
+++ components/esp_adf/esp_adf.cpp
@@ -8,14 +8,11 @@
 #endif
 
 #include "esphome/core/log.h"
-#include "button/esp_adf_button.h" 
 
 namespace esphome {
 namespace esp_adf {
 
 static const char *const TAG = "esp_adf";
-
-ButtonHandler button_handler;
 
 void ESPADF::setup() {
 #ifdef USE_ESP_ADF_BOARD
@@ -23,12 +20,7 @@ void ESPADF::setup() {
   audio_board_handle_t board_handle = audio_board_init();
   audio_hal_ctrl_codec(board_handle->audio_hal, AUDIO_HAL_CODEC_MODE_BOTH, AUDIO_HAL_CTRL_START);
 #endif
-// Call ButtonHandler setup
-  ESP_LOGI(TAG, "Setting up ButtonHandler");
-  button_handler.setup();
-  ESP_LOGI(TAG, "ButtonHandler setup completed");
 }
-
 
 float ESPADF::get_setup_priority() const { return setup_priority::HARDWARE; }
 
