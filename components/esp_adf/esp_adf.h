96844d888574e350fee57b0853225e670f176aef
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-15 19:56:20 -0400

    Update esp_adf.h

--- components/esp_adf/esp_adf.h
+++ components/esp_adf/esp_adf.h
@@ -1,56 +1,28 @@
-#pragma once
+#include "esp_adf.h"
+#include "esphome/core/defines.h"
 
 #ifdef USE_ESP_IDF
 
-#include "esphome/core/component.h"
-#include "esphome/core/helpers.h"
+#ifdef USE_ESP_ADF_BOARD
+#include <board.h>
+#endif
+
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace esp_adf {
 
-static const size_t BUFFER_SIZE = 1024;
+static const char *const TAG = "esp_adf";
 
-enum class TaskEventType : uint8_t {
-  STARTING = 0,
-  STARTED,
-  RUNNING,
-  STOPPING,
-  STOPPED,
-  WARNING = 255,
-};
+void ESPADF::setup() {
+#ifdef USE_ESP_ADF_BOARD
+  ESP_LOGI(TAG, "Start codec chip");
+  audio_board_handle_t board_handle = audio_board_init();
+  audio_hal_ctrl_codec(board_handle->audio_hal, AUDIO_HAL_CODEC_MODE_BOTH, AUDIO_HAL_CTRL_START);
+#endif
+}
 
-struct TaskEvent {
-  TaskEventType type;
-  esp_err_t err;
-};
-
-struct CommandEvent {
-  bool stop;
-};
-
-struct DataEvent {
-  bool stop;
-  size_t len;
-  uint8_t data[BUFFER_SIZE];
-};
-
-class ESPADF;
-
-class ESPADFPipeline : public Parented<ESPADF> {};
-
-class ESPADF : public Component {
- public:
-  void setup() override;
-
-  float get_setup_priority() const override;
-
-  void lock() { this->lock_.lock(); }
-  bool try_lock() { return this->lock_.try_lock(); }
-  void unlock() { this->lock_.unlock(); }
-
- protected:
-  Mutex lock_;
-};
+float ESPADF::get_setup_priority() const { return setup_priority::HARDWARE; }
 
 }  // namespace esp_adf
 }  // namespace esphome
