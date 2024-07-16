82bfe83732e0feb6ad3626feb49e8dbd26b63e00
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-15 21:00:23 -0400

    Update esp_adf_media_player.cpp

--- components/esp_adf/media_player/esp_adf_media_player.cpp
+++ components/esp_adf/media_player/esp_adf_media_player.cpp
@@ -12,11 +12,6 @@ namespace esp_adf {
 
 static const char *TAG = "ESPADFMediaPlayer";
 
-void ESPADFMediaPlayer::register_component() {
-  App.register_component(this);
-  App.register_media_player(this);
-}
-
 void ESPADFMediaPlayer::setup() {
   // Initialize ESP-ADF components
   audio_pipeline_cfg_t pipeline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
